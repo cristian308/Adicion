c5eaa22ff5613b28f66cb150165e04719917abce
Enzo Cremella <enzo_cremella@hotmail.com>
2017-03-31 19:57:01 -0300

    Add files via upload

---
+++ Adicion.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : Adicion.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a,b;
+	cin >> a >> b;
+	cout << a+b << endl;
+	return 0;
+}
